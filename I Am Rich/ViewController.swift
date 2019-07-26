077754d6e86cdf59918f1cb41d544316fe5e1734
dwyfrequency <jdwy215@gmail.com>
2019-07-26 06:31:43 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jack Dwyer on 7/26/19.
+//  Copyright © 2019 Jack Dwyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
